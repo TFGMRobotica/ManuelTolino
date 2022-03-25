71e5043cdace73e1b023414af963285c411d6b2d
ManuelTolino <contreras96.manuel@gmail.com>
2022-03-25 09:31:45 +0100

    Comentario de código y act. readme

--- Noetic_ws/src/roscpptestonly/src/detectmarkers_only.cpp
+++ Noetic_ws/src/roscpptestonly/src/detectmarkers_only.cpp
@@ -5,7 +5,11 @@
 #include <cstdlib>
 
 namespace {
-const char* about = "Detect ArUco marker images";
+
+const char* about = "Deteccion de marcadores ArUco";
+
+//Diccionarios ArUco a definir en línea de comandos
+
 const char* keys  =
         "{d        |16    | dictionary: DICT_4X4_50=0, DICT_4X4_100=1, "
         "DICT_4X4_250=2, DICT_4X4_1000=3, DICT_5X5_50=4, DICT_5X5_100=5, "
@@ -19,6 +23,8 @@ const char* keys  =
 
 int main(int argc, char **argv)
 {
+    /**Funciones de apertura y linea de comandos*/
+
     cv::CommandLineParser parser(argc, argv, keys);
     parser.about(about);
 
@@ -30,8 +36,9 @@ int main(int argc, char **argv)
     int dictionaryId = parser.get<int>("d");
     int wait_time = 10;
     cv::String videoInput = "0";
-    cv::VideoCapture in_video;
-    if (parser.has("v")) {
+
+    cv::VideoCapture in_video; /* Objeto de captura de video OpenCV*/
+    if (parser.has("v")) { /* Si se ha introducido la opcion -v en la linea de comandos*/
         videoInput = parser.get<cv::String>("v");
         if (videoInput.empty()) {
             parser.printMessage();
@@ -49,14 +56,18 @@ int main(int argc, char **argv)
             in_video.set(cv::CAP_PROP_FRAME_HEIGHT, 360);
             in_video.set(cv::CAP_PROP_SATURATION, 0);
         }
-    } else {
+    } else { /* Si no introducimos nada - por ahora usamos esta*/
+        /* descartar si no se usa */
         //in_video.open("udpsrc port=5600 ! application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264 ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink fps-update-interval=1000 sync=false ! appsink drop=1");
+        /* descartar si no se usa */
+
+        /* Se obtiene el vídeo de un streaming mediante gstreamer la configuración en bruto se introduce entre ""*/
         in_video.open("udpsrc port=5600 ! application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264 ! rtph264depay ! avdec_h264 ! videoconvert ! appsink drop=1");
         //in_video.set(cv::CAP_PROP_FPS, 30);
         //in_video.set(cv::CAP_PROP_FRAME_WIDTH, 640);
         //in_video.set(cv::CAP_PROP_FRAME_HEIGHT, 360);
         //in_video.set(cv::CAP_PROP_SATURATION, 0);
-        printf("OPENED");
+        printf("Entrada de vídeo abierta");
     }
 
     if (!parser.check()) {
@@ -65,31 +76,43 @@ int main(int argc, char **argv)
     }
 
     if (!in_video.isOpened()) {
-        std::cerr << "failed to open video input: " << videoInput << std::endl;
+        std::cerr << "No se ha podido iniciar la recepción de vídeo: " << videoInput << std::endl;
         return 1;
     }
 
+    /* Selección del diccionario ArUco */
+    /* Si no se pasa por línea de comandos ¿cual es el por defecto? ToDo*/
     cv::Ptr<cv::aruco::Dictionary> dictionary =
         cv::aruco::getPredefinedDictionary( \
         cv::aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
 
+/* Bucle principal */
     while (in_video.grab()) {
+        /* Elementos matriz de OpenCV, donde se guardan los bits de imagen*/
         cv::Mat image, image_copy;
+        /* Obtiene un frame*/
         in_video.retrieve(image);
+        /* Se copian los bits a una mat auxiliar*/
         image.copyTo(image_copy);
+        /* Vector donde se almacenan los ids de los marcadores detectados*/
         std::vector<int> ids;
+        /* Vector de coordenadas 2D de las esquinas detectadas*/
         std::vector<std::vector<cv::Point2f>> corners;
+        /* Función OpenCV de detección de los marcadores*/
         cv::aruco::detectMarkers(image, dictionary, corners, ids);
         
-        // If at least one marker detected
+        /* Si al menos se ha detectado un marcador...*/
         if (ids.size() > 0)
+        /* Se superpone el contorno del marcador detectado sobre el mismo en la imagen*/
             cv::aruco::drawDetectedMarkers(image_copy, corners, ids);
-
+        /* Se representa en pantalla la imagen obtenida*/
         imshow("Detected markers", image_copy);
+        /* Función para salir del programa con la letra Q. Podría omitirse?*/
         char key = (char)cv::waitKey(wait_time);
         if (key == 27)
             break;
     }
+    /*Si no se detecta vídeo, cerrar objeto de captura de vídeo*/
     in_video.release();
     return 0;
 }
--- README.md
+++ README.md
@@ -2,16 +2,35 @@
 ## Requisitos generales
 El entoro de desarrollo de este repositorio se basa en la plataforma [Dronecode](https://www.dronecode.org/). 
 El desarrollo principalmente se trabaja sobre el stack del autopiloto PX4. Es necesario disponer de todo el 'Toolchain' de desarrollo de la plataforma para poder trabajar con este repositorio. Se encuentra en su carpeta como submódulo la última versión a fecha de 22/03/2022 del stack autopilot PX4, con modificaciones propias. Para poder desarrollar y utilizar este repositorio, se deben seguir las instrucciones proporcionadas en la web de desarrollo de PX4. Se puede trabajar con Ubuntu 18 o Ubuntu 20. Como las versiones de ROS dependen según la versión de Ubuntu. En un primer lugar sólo se va a trabajar con Ubuntu 20. Si hay alguna funcionalidad requerida por la versión anterior, se creará una documentación a parte partiendo desde cero.
-
+*El desarrollo se está realizando sobre Ubuntu 20.4 actualmente*
 1. Instalar ROS Noetic
-2. Seguir los pasos para la instalación de la [Developer Toolchain](https://docs.px4.io/master/en/dev_setup/dev_env_linux_ubuntu.html#rosgazebo) de PX4. 
-
+2. Seguir los pasos para la instalación de PX4: 
+[Developer Toolchain - Ubuntu 20](https://docs.px4.io/master/en/dev_setup/dev_env_linux_ubuntu.html#rosgazebo)
+[Developer Toolchain - Ubuntu 18 y ROS Melodic sólo](https://docs.px4.io/master/en/dev_setup/dev_env_linux_ubuntu.html#rosgazebo) 
+3. Instalar OpenCV 4+
+4. Comprobar el funcionamiento correcto de Gazebo
 Abrir una instancia de QGroundControl y dejar en el background.
 Lanzar el simulador junto el autopiloto:
 ```
 cd PX4-Autopilot
 make px4_sitl gazebo
 ```
+
+## Simulación de control autónomo ([Offboard](https://docs.px4.io/master/en/flight_modes/offboard.html)) en PX4
+```
+Lanzar script de python offboardscript.py
+```
+## Simulación de detección de marcadores [ArUco](https://www.uco.es/investiga/grupos/ava/node/26)
+# Programa de prueba con simulación en Gazebo y Quadrotor IRIS
+Para realizar una prueba de funcionamiento con Gazebo, PX4, y la detección de marcadores ArUco con OpenCV 4 utilizar el nodo **roscpptestonly**
+1. Lanzar QGroundControl en el background
+2. Lanzar PX4 en SITL Gazebo con:
+```
+cd PX4-Autopilot
+make px4_sitl gazebo
+```
+(El Gazebo World por defecto contiene el Iris con una cámara frontal simulada y un marcador ArUco para hacer la prueba, si no es así, pruebe a añadir un marcador ArUco a mano, y utilice la simulación del Typhoon con *make px4_sitl gazebo_typhoon_h480* pues este ya incluye una cámara simulada por defecto)
+3. Lanzar el nodo *roscpptestonly*
 ```
 source /opt/ros/noetic/setup.bash
 cd Noetic_ws
@@ -19,12 +38,11 @@ catkin_make
 source devel/setup.bash
 rosrun roscpptestonly roscpptestonly_node
 ```
+4. Debe aparecer una ventana con el input de vídeo desde el drone de Gazebo. Y si el marcador está enfocado, deben aparecer superpuestos sus contornos de detección.
+Nota: Ruta por defecto de los headers de OpenCV 4 - para VSCode por ejemplo:
 ```
-Lanzar script de python offboardscript.py
+/usr/include/opencv4/
 ```
-## Simulación de control autónomo ([Offboard](https://docs.px4.io/master/en/flight_modes/offboard.html)) en PX4
-
-## Simulación de detección de marcadores [ArUco](https://www.uco.es/investiga/grupos/ava/node/26)
 
 ## Chuleta Github:
 
