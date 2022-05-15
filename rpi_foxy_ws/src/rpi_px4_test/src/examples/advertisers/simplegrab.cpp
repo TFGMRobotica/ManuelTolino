59b75304b8d4f32f834ca81bade768d0e374826b
ManuelTolino <contreras96.manuel@gmail.com>
2022-05-15 15:11:13 +0200

    Added ws for rpi4

---
+++ rpi_foxy_ws/src/rpi_px4_test/src/examples/advertisers/simplegrab.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc,char ** argv)
+{
+  VideoCapture cap(0);         //cap(0);  opens RaspiCam or WebCam
+  //VideoCapture cap("James.mp4"); //cap("James.mp4"); opens the mp4 movie
+  if (!cap.isOpened()) {
+    cerr << "ERROR: Unable to open the camera" << endl;
+    return 0;
+  }
+
+  Mat frame;
+  cout << "Start grabbing, press a key on Live window to terminate" << endl;
+  while(1) {
+    cap >> frame;
+    if (frame.empty()) {
+        cerr << "ERROR: Unable to grab from the camera" << endl;
+        break;
+    }
+    imshow("Live",frame);
+    int key = cv::waitKey(20);
+    key = (key==255) ? -1 : key;
+    if (key>=0)
+      break;
+  }
+
+  cout << "Closing the camera" << endl;
+  cap.release();
+  destroyAllWindows();
+  cout << "bye!" <<endl;
+  return 0;
+}
