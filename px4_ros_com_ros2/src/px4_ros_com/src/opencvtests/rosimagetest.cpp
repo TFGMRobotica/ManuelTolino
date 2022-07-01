a2d5664ef631ef3bc95a717a77000e92d3a5c954
ManuelTolino <contreras96.manuel@gmail.com>
2022-07-01 17:32:26 +0200

    Code clean

--- px4_ros_com_ros2/src/px4_ros_com/src/opencvtests/rosimagetest.cpp
+++ px4_ros_com_ros2/src/px4_ros_com/src/opencvtests/rosimagetest.cpp
@@ -1,8 +1,5 @@
 #/*
-File: sitl_landing_test.cpp
-Author: Manuel Tolino Contreras
-Description: Este programa permite al UAV aterrizar sobre el marcador con id Determinada usando IRLOCKREPORT. (Aproximacion bidimensional)
-Se utiliza sensor de distancia para tener feedback de la altitud y seleccionar así segundo marcador mas pequeño al estar mas bajo.
+File: rosimagetest.cpp
 */
 
 #include <cstdlib>
@@ -11,56 +8,31 @@ Se utiliza sensor de distancia para tener feedback de la altitud y seleccionar a
 #else
 #include <unistd.h>
 #endif
+#include <chrono>
+#include <iostream>
 
-#include <px4_msgs/msg/irlock_report.hpp>
-#include <px4_msgs/msg/timesync.hpp>
-#include <px4_msgs/msg/offboard_control_mode.hpp>
-#include <px4_msgs/msg/trajectory_setpoint.hpp>
-#include <px4_msgs/msg/vehicle_local_position_setpoint.hpp>
-#include <px4_msgs/msg/vehicle_command.hpp>
-#include <px4_msgs/msg/vehicle_control_mode.hpp>
-#include <px4_msgs/msg/vehicle_status.hpp>
-#include <px4_msgs/msg/distance_sensor.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <stdint.h>
 #include <string>
 #include <opencv2/core.hpp> 
 #include <opencv2/opencv.hpp>
-#include <opencv2/aruco.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
-// for compressing the image
-//#include <image_transport/image_transport.hpp>
-//#include <opencv2/imgproc/imgproc.hpp>
-// for compressing the image
 #include <cv_bridge/cv_bridge.h>
 #include <cv_bridge/rgb_colors.h>
 #include <sensor_msgs/image_encodings.hpp>
-//#include <image_transport/image_transport.hpp>
-#include "cv_bridge/cv_bridge.h"
 #include "image_transport/image_transport.hpp"
-#include <opencv2/imgproc/imgproc.hpp>
-
-
-#include <chrono>
-#include <iostream>
 
 using namespace std;
 using namespace cv;
-using namespace std::chrono;
-using namespace std::chrono_literals;
-using namespace px4_msgs::msg;
 using std::placeholders::_1;
 
 /**************** OpenCV global variables *****************/
-//cv_bridge::CvImage::toImageMsg()
- /* Video object */
- //cv::VideoCapture in_video; rpicamera
-cv::VideoCapture in_video;
 
-/**************** OpenCV parameters *****************/
-cv::Mat image /*,image_copy*/;
+cv::VideoCapture in_video;
+cv::Mat image;
 
 /************ ROS2 Node ************/
 
@@ -68,28 +40,10 @@ class DebugVectAdvertiser : public rclcpp::Node
 {
 public:
 	DebugVectAdvertiser() : Node("ros_camera_receiver") {
-
-
-		/*image_sub =
-			this->create_subscription<sensor_msgs::msg::Image>("image_raw", 10,
-				[this](const px4_msgs::msg::Timesync::UniquePtr msg) {
-					timestamp_.store(msg->timestamp);
-				});*/
-		/*image_sub =	this->create_subscription<sensor_msgs::msg::Image>(
-           "image_raw",
-           rclcpp::SensorDataQoS(),
-           std::bind(&DebugVectAdvertiser::imageCallback, image));*/
         image_sub = this->create_subscription<sensor_msgs::msg::Image>(
             "image_raw", 10, std::bind(&DebugVectAdvertiser::imageCallback, this, _1));
-
 	}
 private:
-    /*void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
-        {
-            std::string s = subclass_.hello(msg->data);
-            RCLCPP_INFO(this->get_logger(), "I heard: '%s'", s.c_str());
-        }*/
-    
 	void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr msg) const
 		{
 		try {
@@ -100,19 +54,12 @@ private:
 			RCLCPP_ERROR(logger, "Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
 		}
 		};
-    //rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 	rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub;   
 };
 
 
 int main(int argc, char* argv[])
 {
-    // in_video.open(0); rpicamera
-	
-    //in_video.open("udpsrc port=5600 ! application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264 ! rtph264depay ! avdec_h264 ! videoconvert ! appsink drop=1");
-	// HITL:
-	//in_video.open("-v udpsrc port=5600 caps ='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! rtph264depay ! avdec_h264 ! videoconvert ! fpsdisplaysink sync=false");
-
 	std::cout << "Video input received!" << std::endl;
 	std::cout << "Starting ArUco autoland control node..." << std::endl;
 	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
@@ -120,7 +67,6 @@ int main(int argc, char* argv[])
 	rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<DebugVectAdvertiser>());
 
-    //in_video.release();
     rclcpp::shutdown();
     return 0;
 }
